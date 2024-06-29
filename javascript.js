ee9a6feaad4ea247f474dcfd7a11151e891c1c8d
shakedblank <shakedblank@gmail.com>
2024-06-29 18:05:21 +0300

    added buttons for change size and reset

--- javascript.js
+++ javascript.js
@@ -1,20 +1,59 @@
 const box = document.querySelector("#container");
+const userAsk = document.querySelector("#girdSizer");
+const userDelete = document.querySelector("#girdDelete");
 
 let boxSize= 500;
 let squareTimes=16;
 
-for (let a = 0; a<squareTimes; a++){
- for(let i = 0; i<squareTimes; i++){
-    const square = document.createElement("div");
-    box.style.width= boxSize+"px";
-    box.style.height = box.style.width;
-    square.style.width= (boxSize/squareTimes)+"px";
-    square.style.height= square.style.width;
-    square.classList.toggle("square");
-    square.addEventListener("mouseover",()=>{
-        square.classList.add("highlight");
-    })
-    box.appendChild(square);
- }}
+function gridMaker(size){
+    for (let a = 0; a<size; a++){
+    for(let i = 0; i<size; i++){
+        const square = document.createElement("div");
+        box.style.width= boxSize+"px";
+        box.style.height = box.style.width;
+        square.style.width= (boxSize/size)+"px";
+        square.style.height= square.style.width;
+
+        square.classList.toggle("square");
+        square.addEventListener("mouseover",()=>{
+            square.classList.add("highlight");
+        });
+        box.appendChild(square);
+    }}
+}
+
+gridMaker(squareTimes);
+
+function checkNewSize(){
+    squareTimes= parseInt(prompt("how big should the grid be?"));
+    if(squareTimes>100 ||isNaN(squareTimes)){
+        console.log(squareTimes);
+        console.log(typeof squareTimes);
+        checkNewSize();
+
+    }
+}
+
+function changeGrid(){
+    const squareAll = document.querySelectorAll(".square");
+    checkNewSize();
+    squareAll.forEach((div) =>{
+    box.removeChild(div); 
+    });
+
+   
+   gridMaker(squareTimes);
+}
+
+
+function resetGrid(){
+    const squareAll = document.querySelectorAll(".square");
+    squareAll.forEach((div) =>{
+    div.classList.remove("highlight"); 
+    });
+}
+
+userAsk.addEventListener("click",changeGrid);
+userDelete.addEventListener("click",resetGrid);
 
 
